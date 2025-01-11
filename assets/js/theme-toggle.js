dfd7c37cc03256926d4f499b705281c5c0e9c473
zekejenkins <145144775+zekejenkins@users.noreply.github.com>
2025-01-11 11:30:59 -0500

    Update theme-toggle.js

--- assets/js/theme-toggle.js
+++ assets/js/theme-toggle.js
@@ -1,35 +1,51 @@
 document.addEventListener('DOMContentLoaded', () => {
   const lightIcon = document.getElementById('light-icon');
   const darkIcon = document.getElementById('dark-icon');
+  const themeToggle = document.getElementById('theme-toggle');
+  const skillsGridImages = document.querySelectorAll('.skills-grid img');
+
+  // Function to update the theme
+  function updateTheme(isDarkMode) {
+    if (isDarkMode) {
+      document.body.classList.add('dark-theme');
+      lightIcon.style.display = 'inline';
+      darkIcon.style.display = 'none';
+
+      // Invert icons in the skills grid
+      skillsGridImages.forEach((img) => {
+        img.style.filter = 'invert(1)';
+      });
+    } else {
+      document.body.classList.remove('dark-theme');
+      darkIcon.style.display = 'inline';
+      lightIcon.style.display = 'none';
+
+      // Remove inversion from icons in the skills grid
+      skillsGridImages.forEach((img) => {
+        img.style.filter = 'none';
+      });
+    }
+  }
 
   // Check for saved theme preference in localStorage
   const savedTheme = localStorage.getItem('theme');
   const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
 
-  if (savedTheme === 'dark' || (!savedTheme && prefersDarkScheme)) {
-    document.body.classList.add('dark-theme');
-    darkIcon.style.display = 'none';
-    lightIcon.style.display = 'inline';
-  } else {
-    document.body.classList.remove('dark-theme');
-    lightIcon.style.display = 'none';
-    darkIcon.style.display = 'inline';
-  }
+  const isDarkMode =
+    savedTheme === 'dark' || (!savedTheme && prefersDarkScheme);
+  updateTheme(isDarkMode);
 
   // Toggle theme on click
-  document.getElementById('theme-toggle').addEventListener('click', () => {
-    document.body.classList.toggle('dark-theme');
-    const isDarkMode = document.body.classList.contains('dark-theme');
+  themeToggle.addEventListener('click', () => {
+    const isDarkMode = document.body.classList.toggle('dark-theme');
 
     // Save the user's preference
     if (isDarkMode) {
       localStorage.setItem('theme', 'dark');
-      lightIcon.style.display = 'inline';
-      darkIcon.style.display = 'none';
     } else {
       localStorage.setItem('theme', 'light');
-      darkIcon.style.display = 'inline';
-      lightIcon.style.display = 'none';
     }
+
+    updateTheme(isDarkMode);
   });
 });
